fcecbe8d65bdc929cbae38bb9a5453329dcc42f7
Juan Sifontez <juansfontz@gmail.com>
2021-10-22 17:24:11 -0400

    initial commit

---
+++ scripts/run.js
@@ -0,0 +1,32 @@
+const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners()
+  const waveContractFacory = await hre.ethers.getContractFactory('WavePortal')
+  const waveContract = await waveContractFacory.deploy()
+  await waveContract.deployed()
+
+  console.log("Contract deployed to:", waveContract.address)
+  console.log("Contract deployed by:", owner.address)
+
+  let waveCount = await waveContract.getTotalWaves()
+
+  let waveTxn = await waveContract.wave()
+  await waveTxn.wait()
+
+  waveCount = await waveContract.getTotalWaves()
+
+  waveTxn = await waveContract.connect(randomPerson).wave()
+  await waveTxn.wait()
+
+  waveCount = await waveContract.getTotalWaves()
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    process.exit(1)
+  }
+}
+
+runMain()
